470d8a6a40ad8f5055b2b148787bedf154ed7e49
MKMukeshKannan <mukeshkannan311@gmail.com>
2023-05-19 21:46:40 +0530

    INITIAL COMMIT

---
+++ init.lua
@@ -0,0 +1,8 @@
+require("mkmukeshkannan.plugins-setup")
+require("mkmukeshkannan.core.options")
+require("mkmukeshkannan.core.colorscheme")
+require("mkmukeshkannan.core.keymaps")
+require("mkmukeshkannan.plugins.nvim-tree")
+require("mkmukeshkannan.plugins.lualine")
+require("mkmukeshkannan.plugins.nvim-cmp")
+
---
+++ lua/mkmukeshkannan/core/options.lua
@@ -0,0 +1,30 @@
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+opt.tabstop = 2 
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true 
+
+opt.wrap = false
+
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursor line
+opt.cursorline = true 
+
+-- opt.termguicolors = true
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+opt.splitright = true 
+opt.splitbelow = true
+
+opt.iskeyword:append("-") 
